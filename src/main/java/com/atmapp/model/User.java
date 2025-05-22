a56135e5ac141831d906b748869bf2ce7d79ce8b
Harshit Rai <raiharshit66@gmail.com>
2025-05-22 12:00:47 +0530

    Add the accessors and mutators to the same class

--- src/main/java/com/atmapp/model/User.java
+++ src/main/java/com/atmapp/model/User.java
@@ -11,5 +11,27 @@ public class User {
         this.pin = pin;
     }
 
-   
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
 }
