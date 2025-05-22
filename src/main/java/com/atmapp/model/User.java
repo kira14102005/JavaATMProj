468e1bbdeb09df462762c5231d027374be748ef6
Harshit Rai <raiharshit66@gmail.com>
2025-05-22 12:00:24 +0530

    Similarly create one class for the Users to store their CustomerNumber , PINs etc

---
+++ src/main/java/com/atmapp/model/User.java
@@ -0,0 +1,15 @@
+package com.atmapp.model;
+
+public class User {
+    private int userId;
+    private int customerNumber;
+    private int pin;
+
+    public User(int userId, int customerNumber, int pin) {
+        this.userId = userId;
+        this.customerNumber = customerNumber;
+        this.pin = pin;
+    }
+
+   
+}
